9921cbd134f05f2356b9bc08ded1a47fcaef0242
Serverguyken <serverguyken@gmail.com>
2022-01-03 17:30:59 -0500

    added copyright statements

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,3 +1,42 @@
+#  Copyright (c) 2021, Alden Authors.
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# 3. Neither the name of Alden Org. nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+# File:
+#   Parser/parser.py
+# Author: 
+#   Kehinde Akinsanya
+# Created:
+#   October 28, 2021
+# Description:
+#   This file contains the parser for the language.
+
+
 from os import access, error, path
 from re import split
 from types import NoneType
@@ -3936,7 +3975,17 @@ class Parser:
                 'pos_end': statements.pos_end
             }
             
-          
+        # else:
+        #     print("no finally", self.current_token)
+        #     self.error_detected = True
+        #     return res.failure(self.error['Syntax'](
+        #         {
+        #             'pos_start': start_token.pos_start,
+        #             'pos_end': start_token.pos_end,
+        #             'message': 'attempt statement must have a catch or finally clause',
+        #             'exit': False
+        #         }
+        #     ))    
         
                 
         while self.current_token.type == tokenList.TT_NEWLINE:
